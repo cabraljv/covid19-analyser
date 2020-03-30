01304c46528c746780f587ef922691904a614603
cabraljv <joaovictorcabralsteam@gmail.com>
2020-03-29 22:35:21 -0300

    Updates

---
+++ precache-manifest.acfb5d59ab845853607fb0ff32702fad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60871c9f52929839d881cbf472558bbf",
+    "url": "/covid19-analyser/index.html"
+  },
+  {
+    "revision": "a29f30c8d1d300ad2a75",
+    "url": "/covid19-analyser/static/css/main.08359c5e.chunk.css"
+  },
+  {
+    "revision": "c679d289286e93de9dba",
+    "url": "/covid19-analyser/static/js/2.ebd00f0c.chunk.js"
+  },
+  {
+    "revision": "8c29c59b9267442cb2d538cd4eda51da",
+    "url": "/covid19-analyser/static/js/2.ebd00f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29f30c8d1d300ad2a75",
+    "url": "/covid19-analyser/static/js/main.24333e93.chunk.js"
+  },
+  {
+    "revision": "c46067df2c4bc72bbf24",
+    "url": "/covid19-analyser/static/js/runtime-main.677f8065.js"
+  },
+  {
+    "revision": "21bacc029344fd563f2c09568c522dc7",
+    "url": "/covid19-analyser/static/media/2.21bacc02.png"
+  },
+  {
+    "revision": "4b07aa4068888691878d991edcd3b6c8",
+    "url": "/covid19-analyser/static/media/3.4b07aa40.png"
+  },
+  {
+    "revision": "fd88e843baa238734b4355edfed53dd7",
+    "url": "/covid19-analyser/static/media/biohazard.fd88e843.svg"
+  },
+  {
+    "revision": "02bdc5c81d32bc56333ec8df1280d8ae",
+    "url": "/covid19-analyser/static/media/close.02bdc5c8.svg"
+  }
+]);
